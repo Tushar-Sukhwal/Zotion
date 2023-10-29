73b3328886fa025f6f2cd9d03b03e8c8cfb53dc0
Tushar Sukhwal <tushars7740@gmail.com>
2023-10-29 13:54:59 +0530

    protected routes , navigation page started

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,8 +5,10 @@ import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth } from "convex/react";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -21,7 +23,7 @@ export const Navbar = () => {
     >
       <Logo />{" "}
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2 ">
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <Spinner />}
         {!isAuthenticated && !isLoading && (
           <>
             <SignInButton mode="modal">
@@ -32,6 +34,14 @@ export const Navbar = () => {
             <SignInButton mode="modal">
               <Button size="sm">Get Jotion free</Button>
             </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Zotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
           </>
         )}
         <ModeToggle />
