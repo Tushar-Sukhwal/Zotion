5df17c5e8da57548f6adb8cbd4799676f1139c8c
Tushar Sukhwal <tushars7740@gmail.com>
2023-11-05 02:18:44 +0530

    archive (soft delete ) fuction added(ui remaining)

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,51 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
+export const archive = mutation({
+  args: {
+    id: v.id("documents"),
+  },
+  handler: async (ctx, args) => {
+    const identitiy = await ctx.auth.getUserIdentity();
+
+    if (!identitiy) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identitiy.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Note not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true,
+        });
+        await recursiveArchive(child._id);
+      }
+    };
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+    return true;
+  },
+});
+
 export const getSidebar = query({
   args: {
     parentDocument: v.optional(v.id("documents")),
@@ -22,7 +67,7 @@ export const getSidebar = query({
       .filter((q) => q.eq(q.field("isArchived"), false))
       .order("desc")
       .collect();
- 
+
     return documents;
   },
 });
